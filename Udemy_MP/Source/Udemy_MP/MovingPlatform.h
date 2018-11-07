e3553e1958d131aba18341d824e1e1e67b22ff33
SoloByte <solobyte.games@gmail.com>
2018-11-07 01:25:38 +0100

    Implemented Moving Platform.

---
+++ Udemy_MP/Source/Udemy_MP/MovingPlatform.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMY_MP_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMovingPlatform();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(EditAnywhere)
+	float MovingSpeed = 10.0;
+};
